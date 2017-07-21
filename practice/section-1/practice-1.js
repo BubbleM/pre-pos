704bf6a84ffaee3b95cd53a63751a7761ed4d5b4
BubbleM <1870408473@qq.com>
2017-07-21 21:02:06 +0800

    update parctice1-1 with ES6

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,13 +1,18 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  var result = [];
-  for (let i = 0;i < collectionA.length; i++) {
-    for(let j = 0;j < collectionB.length; j++) {
-      if (collectionA[i] === collectionB[j]){
-        result.push(collectionA[i])
-      }
-    }
-  }
-  return result;
+  /*filter()数组的迭代方法　返回该函数会返回true的项组成的数组*/
+  return collectionA.filter((a) => {
+    if (exist(a, collectionB)) return a;
+  })
 }
+
+/*
+* 查找某元素是否在目标数组中.在返回true,否则返回false
+* @param a 要查找的元素
+* @param collection 目标数组
+* */
+var exist = (a, collection) => {
+  if (collection.indexOf(a) !== -1) return true;
+  return false
+};
