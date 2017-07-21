e0021602cc18f18aff91f7fa93ff3f9c054cf025
BubbleM <1870408473@qq.com>
2017-07-21 21:04:57 +0800

    update practice1-2

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -1,5 +1,12 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+  return collectionA.filter((a) => {
+    if (exist(a, collectionB[0])) return a;
+  })
 }
+
+var exist = (a, collection) => {
+  if (collection.indexOf(a) !== -1) return true;
+  return false
+};
