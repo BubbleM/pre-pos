337500356dfcae6e57dd3c599ce6e40a767bf798
BubbleM <1870408473@qq.com>
2017-07-23 10:31:39 +0800

    update practice3-2

--- practice/section-3/practice-2.js
+++ practice/section-3/practice-2.js
@@ -1,5 +1,24 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  collectionA.map((a) => {
+    if (exist(a.key, objectB)) cut3_1(a);
+  })
+  return collectionA;
 }
+
+/*
+* 对当前对象的count执行满3减1操作
+* @obj 对象形如{key: 'a', count: 3}
+* */
+let cut3_1 = (obj) => {
+  let count = Math.floor(obj.count / 3);
+  for (let i = 0; i < count; i++) {
+    obj.count--;
+  }
+}
+
+let exist = (a, object) => {
+  if (object.value.indexOf(a) !== -1) return true;
+  return false
+};
