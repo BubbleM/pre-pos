6b9a460fc383859aa46151cb49ee2dc1bfb28f48
BubbleM <1870408473@qq.com>
2017-07-23 10:16:40 +0800

    update practice3-1

--- practice/section-3/practice-1.js
+++ practice/section-3/practice-1.js
@@ -1,5 +1,20 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  /*遍历collectionA中的每一项,如果当前项的key存在与objectB.value中　对其count项减１*/
+  collectionA.map((a) => {
+    if (exist(a.key, objectB)) a.count--;
+  })
+  return collectionA;
 }
+
+
+/*
+* 查找a是否在对象数组中　如果在返回true否则返回false
+* @param a 查找值
+* @param object 对象数组 如{value: ['a', 'd', 'e', 'f']};
+* */
+var exist = (a, object) => {
+  if (object.value.indexOf(a) !== -1) return true;
+  return false
+};
